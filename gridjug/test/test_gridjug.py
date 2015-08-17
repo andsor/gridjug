08947555898200902ecb1e5bcab41cf321782a3c
Andreas Sorge <as@asorge.de>
2015-08-17 14:58:05 +0200

    TST Ensure local execution of GridMap

--- gridjug/test/test_gridjug.py
+++ gridjug/test/test_gridjug.py
@@ -28,30 +28,30 @@ def jugdir():
 
 
 def test_execute():
-    gridjug.grid_jug(jugfile=PRIMES_JUGFILE)
+    gridjug.grid_jug(jugfile=PRIMES_JUGFILE, local=True)
 
 
 def test_execute_jugdir():
     shutil.rmtree(PRIMES_JUGDIR, ignore_errors=True)
     assert not os.path.isdir(PRIMES_JUGDIR)
-    gridjug.grid_jug(jugfile=PRIMES_JUGFILE)
+    gridjug.grid_jug(jugfile=PRIMES_JUGFILE, local=True)
     assert os.path.isdir(PRIMES_JUGDIR)
 
 
 def test_execute_specify_jugdir(jugdir):
     assert os.path.isdir(jugdir)
     assert not os.listdir(jugdir)  # empty directory
-    gridjug.grid_jug(jugfile=PRIMES_JUGFILE, jugdir=jugdir)
+    gridjug.grid_jug(jugfile=PRIMES_JUGFILE, jugdir=jugdir, local=True)
     assert os.listdir(jugdir)  # non-empty directory
 
 
 def test_init_jugspace(jugdir):
-    gridjug.grid_jug(jugfile=PRIMES_JUGFILE, jugdir=jugdir)
+    gridjug.grid_jug(jugfile=PRIMES_JUGFILE, jugdir=jugdir, local=True)
     jug.init(jugfile=PRIMES_JUGFILE, jugdir=jugdir)
 
 
 def test_access_results(jugdir):
-    gridjug.grid_jug(jugfile=PRIMES_JUGFILE, jugdir=jugdir)
+    gridjug.grid_jug(jugfile=PRIMES_JUGFILE, jugdir=jugdir, local=True)
     _, jugspace = jug.init(jugfile=PRIMES_JUGFILE, jugdir=jugdir)
     assert jug.value(jugspace['primes10']) == [
         True, True, False, True, False, True, False, False, False
