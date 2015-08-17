008f813b28de861431c331a6afc5d5f7b7008d1c
Andreas Sorge <as@asorge.de>
2015-08-17 20:44:11 +0200

    TST Finish NLD cluster tests

--- gridjug/test/test_gridjug.py
+++ gridjug/test/test_gridjug.py
@@ -3,16 +3,11 @@
 import inspect
 import os
 import shutil
+import tempfile
 
 import gridjug
 import jug
 import pytest
-
-try:
-    from tempfile import TemporaryDirectory  # Python >= 3.2
-except ImportError:  # Python < 3.2
-    # http://stackoverflow.com/a/19299884/2366781
-    from _tempfile import TemporaryDirectory
 
 
 # http://stackoverflow.com/a/50905/2366781
@@ -28,15 +23,19 @@ ON_NLD_LOGIN = ON_NLD_CLUSTER and (
     os.environ.get('CLUSTERNAME', None) == 'login'
 )
 
+if ON_NLD_LOGIN:
+    TEMP_DIR = os.path.join(THIS_DIR, 'tmp')
+    if not os.path.isdir(TEMP_DIR):
+        os.mkdir(TEMP_DIR)
+
 NLD_GRIDMAP_PARAMS = {
-    'queue': 'frigg.q,skadi.q,navier.q',
+    'local': False,
+    'temp_dir': TEMP_DIR,
+    'quiet': False,
+    'queue': 'frigg.q,skadi.q',
+    'interpreting_shell': '/bin/bash',
+    'copy_env': False,
 }
-
-
-@pytest.yield_fixture
-def jugdir():
-    with TemporaryDirectory() as ret:
-        yield ret
 
 
 def test_execute():
@@ -44,32 +43,97 @@ def test_execute():
 
 
 def test_execute_jugdir():
-    shutil.rmtree(PRIMES_JUGDIR, ignore_errors=True)
-    assert not os.path.isdir(PRIMES_JUGDIR)
     gridjug.grid_jug(jugfile=PRIMES_JUGFILE, local=True)
     assert os.path.isdir(PRIMES_JUGDIR)
 
 
-def test_execute_specify_jugdir(jugdir):
-    assert os.path.isdir(jugdir)
-    assert not os.listdir(jugdir)  # empty directory
-    gridjug.grid_jug(jugfile=PRIMES_JUGFILE, jugdir=jugdir, local=True)
-    assert os.listdir(jugdir)  # non-empty directory
+def test_execute_specify_jugdir(tmpdir):
+    jugdir = tmpdir
+    assert jugdir.ensure_dir()
+    assert not jugdir.listdir()  # empty directory
+    gridjug.grid_jug(
+        jugfile=PRIMES_JUGFILE, jugdir=jugdir.strpath, local=True,
+    )
+    assert jugdir.listdir()  # non-empty directory
 
 
-def test_init_jugspace(jugdir):
-    gridjug.grid_jug(jugfile=PRIMES_JUGFILE, jugdir=jugdir, local=True)
-    jug.init(jugfile=PRIMES_JUGFILE, jugdir=jugdir)
+def test_init_jugspace(tmpdir):
+    jugdir = tmpdir
+    gridjug.grid_jug(
+        jugfile=PRIMES_JUGFILE, jugdir=jugdir.strpath, local=True,
+    )
+    jug.init(jugfile=PRIMES_JUGFILE, jugdir=jugdir.strpath)
 
 
-def test_access_results(jugdir):
-    gridjug.grid_jug(jugfile=PRIMES_JUGFILE, jugdir=jugdir, local=True)
-    _, jugspace = jug.init(jugfile=PRIMES_JUGFILE, jugdir=jugdir)
+def test_access_results(tmpdir):
+    jugdir = tmpdir
+    gridjug.grid_jug(
+        jugfile=PRIMES_JUGFILE, jugdir=jugdir.strpath, local=True,
+    )
+    _, jugspace = jug.init(jugfile=PRIMES_JUGFILE, jugdir=jugdir.strpath)
     assert jug.value(jugspace['primes10']) == [
         True, True, False, True, False, True, False, False, False
     ]
 
 
-@pytest.mark.skipif(not ON_NLD_LOGIN)
+@pytest.mark.skipif(not ON_NLD_LOGIN, reason='Not on NLD cluster login node')
+def test_gridmap_has_drmaa():
+    import drmaa
+
+
+@pytest.mark.skipif(not ON_NLD_LOGIN, reason='Not on NLD cluster login node')
 def test_nld_execute():
-    gridjug.grid_jug(jugfile=PRIMES_JUGFILE, local=False)
+    gridjug.grid_jug(
+        jugfile=PRIMES_JUGFILE,
+        **NLD_GRIDMAP_PARAMS
+    )
+
+
+@pytest.mark.skipif(not ON_NLD_LOGIN, reason='Not on NLD cluster login node')
+def test_nld_execute_jugdir():
+    gridjug.grid_jug(
+        jugfile=PRIMES_JUGFILE,
+        **NLD_GRIDMAP_PARAMS
+    )
+    assert os.path.isdir(PRIMES_JUGDIR)
+
+
+@pytest.fixture
+def jugdir():
+    ret = tempfile.mkdtemp(suffix='.jugdata', dir=TEMP_DIR)
+    assert os.path.isdir(ret)
+    assert not os.listdir(ret)  # empty directory
+    return ret
+
+
+@pytest.mark.skipif(not ON_NLD_LOGIN, reason='Not on NLD cluster login node')
+def test_nld_execute_specify_jugdir(jugdir):
+    gridjug.grid_jug(
+        jugfile=PRIMES_JUGFILE,
+        jugdir=jugdir,
+        **NLD_GRIDMAP_PARAMS
+    )
+    assert os.listdir(jugdir)  # non-empty directory
+
+
+@pytest.mark.skipif(not ON_NLD_LOGIN, reason='Not on NLD cluster login node')
+def test_nld_init_jugspace(jugdir):
+    gridjug.grid_jug(
+        jugfile=PRIMES_JUGFILE,
+        jugdir=jugdir,
+        **NLD_GRIDMAP_PARAMS
+    )
+    jug.init(jugfile=PRIMES_JUGFILE, jugdir=jugdir)
+
+
+@pytest.mark.skipif(not ON_NLD_LOGIN, reason='Not on NLD cluster login node')
+def test_access_results(jugdir):
+    gridjug.grid_jug(
+        jugfile=PRIMES_JUGFILE,
+        jugdir=jugdir,
+        **NLD_GRIDMAP_PARAMS
+    )
+    _, jugspace = jug.init(jugfile=PRIMES_JUGFILE, jugdir=jugdir)
+    assert jug.value(jugspace['primes10']) == [
+        True, True, False, True, False, True, False, False, False
+    ]
